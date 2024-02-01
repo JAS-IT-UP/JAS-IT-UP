591d0ffe1fd2b2f2262c43d276a130f43d146a56
Angiegru <angiegrullon01@gmail.com>
2024-02-01 15:22:39 -0500

    Added columns to users table

--- react-express-auth-main/src/db/migrations/20230423034706_create_users_table.js
+++ react-express-auth-main/src/db/migrations/20230423034706_create_users_table.js
@@ -4,7 +4,11 @@
  */
 exports.up = (knex) => knex.schema.createTable('users', (table) => {
   table.increments();
+  table.string('profile_picture').notNullable();
+  table.string('first_name').notNullable();
+  table.string('last_name').notNullable();
   table.string('username').notNullable().unique();
+  table.string('email').notNullable();
   table.string('password_hash').notNullable();
   table.timestamps(true, true);
 });
