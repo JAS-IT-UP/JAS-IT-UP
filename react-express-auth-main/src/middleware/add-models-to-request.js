d336921651efa3d320c4eb3f62167d1a7e9f07f1
nathanielrobinson24 <n.rbinson@aol.com>
2024-02-05 10:32:01 -0500

    Edit of auth-utils.js

--- react-express-auth-main/src/middleware/add-models-to-request.js
+++ react-express-auth-main/src/middleware/add-models-to-request.js
@@ -1,8 +1,16 @@
-const User = require('../db/models/user');
+const users = require('../db/models/user.js');
+const posts = require('../db/models/post.js');
+const saved_posts = require('../db/models/savedPost.js');
+const post_materials = require('../db/models/postMaterial.js');
+const materials = require('../db/models/material.js');
 
 const addModelsToRequest = (req, res, next) => {
   req.db = {
-    User,
+    users, 
+    posts,
+    saved_posts,
+    post_materials,
+    materials
   };
   next();
 };
