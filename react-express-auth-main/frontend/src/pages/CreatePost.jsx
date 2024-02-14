ec70abd1d99029103afe159fb95cc606f5505483
SirreaReeves <reevessirrea@gmail.com>
2024-02-14 18:07:28 -0500

    PostCard component added to html

--- react-express-auth-main/frontend/src/pages/CreatePost.jsx
+++ react-express-auth-main/frontend/src/pages/CreatePost.jsx
@@ -24,7 +24,6 @@ export default function CreatePostPage() {
 
       
     const [post, error] = await createPost(formData); 
-    console.log(post,error)
 
     if(error){
         return setErrorText(error.message)
@@ -43,26 +42,36 @@ export default function CreatePostPage() {
   };
 
   return (
+    <div>
     <form onSubmit={handleSubmit} onChange={handleChange} aria-labelledby="create-heading">
-      {/* <h1 id= "create-heading">Picture:</h1> */}
       <div id= "picture-section"> 
-      <label htmlFor="image"> <h1>Picture:</h1> </label>
-      <input type="" name="postPicture" id="image" except="image/*" placeholder="Add An Image Of Your Finished Project Here" value={formData.postPicture} onChange={handleImageChange} required></input>
+      <label htmlFor="image"> <h1 id="create-heading">Picture:</h1> </label>
+      <input type="" name="postPicture" id="image" except="image/*" placeholder="Add An Image Of Your Finished Project Here" 
+      onChange={handleChange} 
+      value={formData.postPicture} required></input>
+      {/* <img src={formData.postPicture} alt="" /> */}
       </div>
 
       <div className="materials-section"> 
-      <label htmlFor="materials"> <h1>Materials:</h1></label> 
+      <label htmlFor="materials"> <h1 id="materials">Materials:</h1></label> 
       
       <Dropdown/>
       </div>
 
       <div className="description-section"> 
       <label htmlFor="description"> <h1> The Revamp:</h1></label> 
-      <textarea type="text" id="description" name="projectDescription" placeholder="This Is Where You Help Others JAS UP The Materials They Have. Give Us A Step By Step Description Of Your Project." value={formData.projectDescription} onChange={handleChange} required></textarea>
+      <textarea type="text" id="description" name="projectDescription" placeholder="This Is Where You Help Others JAS UP The Materials They Have. Give Us A Step By Step Description Of Your Project." onChange={handleChange} 
+      value={formData.projectDescription}  required></textarea>
       </div>
 
       <button type="submit">POST</button>
     </form>
+    <div>
+    {posts.map((post, index) => (
+      <PostCard key={index} postPicture={post.postPicture} projectDescription={post.projectDescription} />
+    ))}
+    </div>
+    </div>
   );
 }
 
