0cd80c91aab8beaab14f1c5afe093a7ee2fbfc8b
Angiegru <angiegrullon01@gmail.com>
2024-02-22 17:01:17 -0500

    Explore Component, Hamburger Bar use syntax corrected

--- react-express-auth-main/frontend/src/components/ExplorePostCard.jsx
+++ react-express-auth-main/frontend/src/components/ExplorePostCard.jsx
@@ -1,18 +1,12 @@
 import React, { useContext, useState } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
-import { Squash as Hamburger } from 'hamburger-react';
+import  Hamburger from 'hamburger-react';
 
 export default function ExplorePostCard({ postPicture, projectDescription, materialName, id, username, profilePicture}) {
     const { currentUser } = useContext(CurrentUserContext);
     const [isOpen, setOpen] = useState(false);
 
-    const handleToggle = () => {
-        setOpen(!isOpen);
-    };
-
-    // const handleSaveToggle = () => {
-    //     if ()
-    // }
+    <Hamburger toggled={isOpen} toggle={setOpen} />
 
 
     return (
@@ -20,21 +14,26 @@ export default function ExplorePostCard({ postPicture, projectDescription, mater
             {/* <img src={profilePicture} alt={`Posted by ${username}`}/> */}
             <img src={postPicture} alt={`Post ${id}`} />
             <div className="post-interactions">
-                <Hamburger toggled={isOpen} onClick={handleToggle} />
-                {isOpen && (
-                    <>
-                        <h3>Materials:</h3>
-                        <ul>
-                            <li>
-                                {materialName}
-                            </li>
-                        </ul>
-                        <div>
-                            <h3>The Revamp:</h3>
-                            <p>{projectDescription}</p>
-                        </div>
-                    </>
-                )}
+                <Hamburger onToggle={toggled => {
+                    if(toggled){
+                        (
+                            <>
+                                <h3>Materials:</h3>
+                                <ul>
+                                    <li>
+                                        {materialName}
+                                    </li>
+                                </ul>
+                                <div>
+                                    <h3>The Revamp:</h3>
+                                    <p>{projectDescription}</p>
+                                </div>
+                            </>
+                        )
+                    } else {
+                        //close a menu
+                    }
+                }} />
             </div>
         </section>
     );
