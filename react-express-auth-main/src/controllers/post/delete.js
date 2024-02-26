30d1765806adb262892f2b114a524dd4e493154b
SirreaReeves <reevessirrea@gmail.com>
2024-02-25 21:00:54 -0500

    Router file, delete button functionality, user post fun ctionality

--- react-express-auth-main/src/controllers/post/delete.js
+++ react-express-auth-main/src/controllers/post/delete.js
@@ -1,20 +1,20 @@
 const deletePost = async (req, res) => {
-    const {
-      db: { Post },
-      params: { id },
-    } = req;
-    console.log(id, "this is the id")
-    try {
-      const post = await Post.delete(Number(id));
-      if (!post) {
-        // console.log(post, "this is the post")
-        return res.status(404).json({ message: "Post not found" });
-      }
-      return res.json({ message: "Post deleted" });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ message: "Failed to delete post" });
+  const {
+    db: { Post },
+    params: { id },
+  } = req;
+  console.log(id, "this is the id")
+  try {
+    const post = await Post.delete(Number(id));
+    if (!post) {
+      // console.log(post, "this is the post")
+      return res.status(404).json({ message: "Post not found" });
     }
-  };
-  
-  module.exports = deletePost;
+    return res.json({ message: "Post deleted" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Failed to delete post" });
+  }
+};
+
+module.exports = deletePost;
