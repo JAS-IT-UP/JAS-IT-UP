87e9411a245d011f0cb151f14b36ce7151bdb742
SirreaReeves <reevessirrea@gmail.com>
2024-02-29 05:47:28 -0500

    Added materials to dropdown

--- react-express-auth-main/frontend/src/components/DropDown.jsx
+++ react-express-auth-main/frontend/src/components/DropDown.jsx
@@ -4,7 +4,7 @@ import { useState, useContext } from "react";
 export default function Dropdown({ materialName, materialId }) {
 
 	const [materials, setMaterials] = useState([]);
-	const [selectedMaterial, setSelectedMaterial] = useState(materialName);
+	// const [selectedMaterial, setSelectedMaterial] = useState(materialName);
 
 	const  handleDropdownChange = (event) => {
 		const value = event.target.value;
@@ -14,35 +14,26 @@ export default function Dropdown({ materialName, materialId }) {
 return  (
 	<div >
 		<label id="dropdown">
-			{/* <h3>Choose Your Repurposed Materials:</h3> */}
 				 <select style={{ width: '400px', height: '80px', borderRadius: '25px', textAlign: 'center', fontSize:'13px'}} onChange={handleDropdownChange}>
-				<option  value={materialName}>Milk Carton</option>
+				<option  value={materialId}>Milk Carton</option>
 				<option  value={materialId}>Jeans</option>
 				<option  value={materialId}>Mason Jars</option>
                 <option  value={materialId}>NewsPaper/Magazine</option>
                 <option  value={materialId}>Fabric Scraps</option>
                 <option  value={materialId}>Cans</option>
+				<option value={materialId}>Yogurt Cups</option>
+				<option value={materialId}>Records</option>
+				<option value={materialId}>Pens</option>
+				<option value={materialId}>Crayons</option>
+				<option value={materialId}>Candles</option>
+				<option value={materialId}>Cups</option>
+				<option value={materialId}>Ribbons/Ties</option>
+				<option value={materialId}>Glass Bottles</option>
+				<option value={materialId}>Cardboard</option>
+				<option value={materialId}>Plastic Bags</option>
                 <option  value="Other">Other</option>
 			</select>
 		</label>
-		{/* <p>Selected Material: {materialName}</p> */}
 	</div>
 	);
-
-// return  (
-// 	<div>
-// 	<label>
-// 		<h3>Choose Your Repurposed Materials:</h3>
-// 		<select onChange={handleDropdownChange} value={selectedMaterial}>
-// 			<option value="">Select a material</option>
-// 			{materials.map((material) => (
-// 				<option key={materialId} value={material.id}>
-// 					{material.materialName}
-// 				</option>
-// 			))}
-// 		</select>
-// 	</label>
-// 	<p>Selected Material: {selectedMaterial}</p>
-// 	</div>
-// 		);
 }
