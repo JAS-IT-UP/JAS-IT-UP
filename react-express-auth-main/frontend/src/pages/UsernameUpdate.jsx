609c70b019774233dd75f70e9497a29170853e9b
SirreaReeves <reevessirrea@gmail.com>
2024-03-01 01:25:45 -0500

    Update username form

--- react-express-auth-main/frontend/src/pages/UsernameUpdate.jsx
+++ react-express-auth-main/frontend/src/pages/UsernameUpdate.jsx
@@ -6,36 +6,22 @@ import UpdateUsernameForm from "../components/UpdateUsernameForm";
 import CurrentUserContext from "../contexts/current-user-context";
 export default function UsernameUpdate() {
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-  // const [userProfile, setUserProfile] = useState(null);
   const { id } = useParams();
   const isCurrentUserProfile = currentUser && currentUser.id === Number(id);
-  // console.log(currentUser, "this is isCurrentUserProfile");
+
   const navigate = useNavigate();
 
-  // const createPostButton = (
-  //   <button id="create" onClick={() => navigate("/create-post")}>
-  //     +
-  //   </button>
-  // );
 
   return (<>
     <div id="username-back">
-    {/* <div id="user-picture">
-          {createPostButton}
-          <div id="picture">
-            <img
-              id="user-profile-picture"
-              src={currentUser.profilePicture}
-              alt=""
-            />
-          </div>
-        </div> */}
-
+    <div id="update-background">
 
       <h1 id="update-username" >Username Update</h1>
       {currentUser && (
         <UpdateUsernameForm currentUser={currentUser} setCurrentUser={setCurrentUser} />
       )}
+    </div>
+
     </div>
   </>);
 }
